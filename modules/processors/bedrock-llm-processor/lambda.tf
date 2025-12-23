976fabc9776caa049607bcce47ceae377dbb6f28
Damian Kuroczko <kuroczd@amazon.com>
2025-12-23 12:18:57 +0100

    fix(classification) Decrease classification Lambda memory_size from 4096 MB to 3008 MB

--- modules/processors/bedrock-llm-processor/lambda.tf
+++ modules/processors/bedrock-llm-processor/lambda.tf
@@ -54,7 +54,7 @@ resource "aws_lambda_function" "classification" {
   handler       = "index.handler"
   runtime       = "python3.12"
   timeout       = 900
-  memory_size   = 4096
+  memory_size   = 3008
 
   filename         = data.archive_file.classification_lambda.output_path
   source_code_hash = data.archive_file.classification_lambda.output_base64sha256
